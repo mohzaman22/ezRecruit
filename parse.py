65dbb6b15c550bf27f1ede5a79db7dd94933681d
Donnell <donnedebnam1@aol.com>
2019-01-10 10:33:06 -0500

    refactoring

--- parse.py
+++ parse.py
@@ -1,11 +1,18 @@
 import os
+import scrape
 import textract
 
-def getcontent(file):
-    return textract.process(('Resumes/{}').format(file))
-
 def main():
-
+    applicants = {}
     for file in os.listdir('Resumes'):
         # Extract content of Resume
-        pdfText = getcontent(file)
+        pdfText = scrape.getcontent(file)
+        # Resume Author
+        content = re.findall('\w+',pdfText)
+        candidate = scrape.getname(content)
+        # Process skill matches and assign a score to this candidate;
+        # store entry in dictionary to be analyzed after processing
+        # all candidates.
+    return applicants
+
+if __name__ == '__main__': main()
