9b0418d66257c8cb903644b2b41207decbd3f964
Donnell <donnedebnam1@aol.com>
2019-01-15 20:13:56 -0500

    renaming and skill extraction

--- parse.py
+++ parse.py
@@ -21,21 +21,17 @@ def convert(fname, pages=None):
         pagenums = set()
     else:
         pagenums = set(pages)
-
     output = StringIO()
     manager = PDFResourceManager()
     converter = TextConverter(manager, output, laparams=LAParams())
     interpreter = PDFPageInterpreter(manager, converter)
-
     infile = file('Resumes/{}'.format(fname),'rb')
     for page in PDFPage.get_pages(infile, pagenums):
         interpreter.process_page(page)
-
     infile.close()
     converter.close()
     text = output.getvalue()
     output.close
-
     return text
 
 def extract_name(text):
@@ -47,10 +43,34 @@ def extract_name(text):
         if ent.label_ == 'PER':
             return ent.text
 
+def extract_skills(lookup,text):
+    skills = []
+    for word in text.split(' '):
+        if word in lookup.keys():
+            skills.append(word)
+    return skills
+
 def main():
+    # Reads CSV and creates dictionary of technical skills and their skills type.
+    skills_dict = {}
+    with open('techskills.csv','r') as ts:
+        for skill in ts:
+            line = skill.split(',')
+            skills_dict[line[1].replace('\r\n','')] = line[0]
+    # Parse each candidate's resume.
     for f in os.listdir('Resumes'):
         if f != '.DS_Store':
-            res = convert(f)
-            print extract_name(res)
+            # Convert to raw text
+            text = convert(f)
+            name = extract_name(text).rstrip()
+            text = text.lower()
+            # Candidate name
+            print 'Candidate: {}'.format(name) 
+            # Contact information
+            print 'Email: ' + re.findall('\S+@\S+', text)[0]
+            # Technical skills
+            print 'Technical skills: '
+            print extract_skills(skills_dict,text)
+            print '\n'
 
 if __name__ == '__main__': main()
